461d893885324f81278442f315b4b012978c1337
daqians <daqians123@163.com>
2022-03-29 20:03:39 +0200

    new update

---
+++ models/modules/backbone/VIT.py
@@ -0,0 +1,87 @@
+import torch
+from torch import nn
+
+from einops import repeat
+from einops.layers.torch import Rearrange
+
+from Low_level_CV_PPL.models.modules.Attention.TransformerBlock import Transformer
+
+
+def pair(t):
+    return t if isinstance(t, tuple) else (t, t)
+
+
+# class Transformer(nn.Module):
+#     def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+#         super().__init__()
+#         self.layers = nn.ModuleList([])
+#         for _ in range(depth):
+#             self.layers.append(nn.ModuleList([
+#                 PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
+#                 PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
+#             ]))
+#     def forward(self, x):
+#         for attn, ff in self.layers:
+#             x = attn(x) + x
+#             x = ff(x) + x
+#         return x
+
+class ViT(nn.Module):
+    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool = 'cls', channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0.):
+        super().__init__()
+        image_height, image_width = pair(image_size)
+        patch_height, patch_width = pair(patch_size)
+
+        assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
+
+        num_patches = (image_height // patch_height) * (image_width // patch_width)
+        patch_dim = channels * patch_height * patch_width
+        assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
+
+        self.to_patch_embedding = nn.Sequential(
+            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width),
+            nn.Linear(patch_dim, dim),
+        )
+
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
+        self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
+        self.dropout = nn.Dropout(emb_dropout)
+
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
+
+        self.pool = pool
+        self.to_latent = nn.Identity()
+
+        self.mlp_head = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, num_classes)
+        )
+
+    def forward(self, img):
+        x = self.to_patch_embedding(img)
+        b, n, _ = x.shape
+
+        cls_tokens = repeat(self.cls_token, '() n d -> b n d', b = b)
+        x = torch.cat((cls_tokens, x), dim=1)
+        x += self.pos_embedding[:, :(n + 1)]
+        x = self.dropout(x)
+        x = self.transformer(x)
+        x = x.mean(dim = 1) if self.pool == 'mean' else x[:, 0]
+        x = self.to_latent(x)
+        return self.mlp_head(x)
+
+if __name__ == "__main__":
+
+    model = ViT(
+        dim = 64,           # initial dimensions after input projection, which increases by 2x each stage
+        depth = 1,         # number of attention steps
+        heads = 8,     # number of transformer blocks per stage
+        mlp_dim = 128,   # set window size (along one side) for which to do the attention within
+        image_size = 256,
+        patch_size = 8,
+        num_classes = 1000
+    )
+
+    x = torch.randn(10, 3, 256, 256)
+    pred = model(x) # (1, 3, 256, 256)
+    print(pred.shape)
